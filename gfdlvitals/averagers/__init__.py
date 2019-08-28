d2040498b9e31b3f4b55d7395952f478255da4f7
John Krasting <John.Krasting@noaa.gov>
2019-08-28 10:42:48 -0400

    Updates to cubesphere averaging utility
    - Changed interface to accept a list of data tiles
    - Added multiprocessing support

--- gfdlvitals/averagers/__init__.py
+++ gfdlvitals/averagers/__init__.py
@@ -1,6 +1,7 @@
+from . import cubesphere
 from . import ice
 from . import latlon
 from . import land_lm3
 from . import tripolar
 
-__all__ = ['ice','land_lm3','latlon','tripolar']
+__all__ = ['cubesphere','ice','land_lm3','latlon','tripolar']
--- gfdlvitals/averagers/cubesphere.py
+++ gfdlvitals/averagers/cubesphere.py
@@ -1,36 +1,55 @@
-import gmeantools
-import netCDF4 as nc
 import numpy as np
-import sqlite3
-import sys
+import multiprocessing
 
-fYear = sys.argv[1]
-outdir = sys.argv[2]
-label = sys.argv[3]
-history = str(sys.argv[4]).split(',')
+import gfdlvitals.util.gmeantools as gmeantools
 
-gs_tiles = []
-for tx in range(1,7): gs_tiles.append(gmeantools.ncopen(fYear + '.grid_spec.tile'+str(tx)+'.nc'))
+__all__ = ['process_var','average']
 
-geoLat = gmeantools.cube_sphere_aggregate('grid_latt',gs_tiles)
-geoLon = gmeantools.cube_sphere_aggregate('grid_lont',gs_tiles)
-cellArea = gmeantools.cube_sphere_aggregate('area',gs_tiles)
+def process_var(varName):
+     units     = gmeantools.extract_metadata(data_tiles[0],varName,'units')
+     long_name = gmeantools.extract_metadata(data_tiles[0],varName,'long_name')
+     if (len(data_tiles[0].variables[varName].shape) == 3):
+       var = gmeantools.cube_sphere_aggregate(varName,data_tiles)
+       var = np.ma.average(var,axis=0,
+           weights=data_tiles[0].variables['average_DT'][:])
+       for reg in ['global','tropics','nh','sh']:
+         result, areaSum = gmeantools.area_mean(var,cellArea,
+             geoLat,geoLon,region=reg)
+         sqlfile = outdir+'/'+fYear+'.'+reg+'Ave'+label+'.db'
+         gmeantools.write_metadata(sqlfile,varName,'units',units)
+         gmeantools.write_metadata(sqlfile,varName,'long_name',long_name)
+         gmeantools.write_sqlite_data(sqlfile,varName,fYear[:4],result)
+         gmeantools.write_sqlite_data(sqlfile,'area',fYear[:4],areaSum)
 
-for atmosFile in history:
-    data_tiles = []
-    for tx in range(1,7): data_tiles.append(gmeantools.ncopen(fYear + '.'+atmosFile+'.tile'+str(tx)+'.nc'))
-    for varName in data_tiles[0].variables.keys():
-      units     = gmeantools.extract_metadata(data_tiles[0],varName,'units')
-      long_name = gmeantools.extract_metadata(data_tiles[0],varName,'long_name')
-      if (len(data_tiles[0].variables[varName].shape) == 3):
-        var = gmeantools.cube_sphere_aggregate(varName,data_tiles)
-        var = np.ma.average(var,axis=0,weights=data_tiles[0].variables['average_DT'][:])
-        for reg in ['global','tropics','nh','sh']:
-          result, areaSum = gmeantools.area_mean(var,cellArea,geoLat,geoLon,region=reg)
-          sqlfile = outdir+'/'+fYear+'.'+reg+'Ave'+label+'.db'
-          gmeantools.write_metadata(sqlfile,varName,'units',units)
-          gmeantools.write_metadata(sqlfile,varName,'long_name',long_name)
-          gmeantools.write_sqlite_data(sqlfile,varName,fYear[:4],result)
-          gmeantools.write_sqlite_data(sqlfile,'area',fYear[:4],areaSum)
 
-exit()
+def average(gs_tl,da_tl,year,out,lab):
+    global gs_tiles
+    global data_tiles
+    global fYear
+    global outdir
+    global label
+
+    gs_tiles = gs_tl
+    data_tiles = da_tl
+    fYear = year
+    outdir = out
+    label = lab
+
+    global geoLon
+    global geoLat
+    global cellArea
+
+    geoLat = gmeantools.cube_sphere_aggregate('grid_latt',gs_tiles)
+    geoLon = gmeantools.cube_sphere_aggregate('grid_lont',gs_tiles)
+    cellArea = gmeantools.cube_sphere_aggregate('area',gs_tiles)
+
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool.map(process_var,data_tiles[0].variables.keys())
+
+#gs_tiles = []
+#for tx in range(1,7): gs_tiles.append(gmeantools.ncopen(fYear + '.grid_spec.tile'+str(tx)+'.nc'))
+#
+#for atmosFile in history:
+#    data_tiles = []
+#    for tx in range(1,7): data_tiles.append(gmeantools.ncopen(fYear + '.'+atmosFile+'.tile'+str(tx)+'.nc'))
+
