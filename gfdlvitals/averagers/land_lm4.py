Fixed pylint errors for atmos and land averagers

gfdlvitals/averagers/land_lm4.py
@@ -1,23 +1,27 @@
 """Land LM4.1 Averaging Routines"""
 
-import multiprocessing
-import re
-
-from functools import partial
-
-import numpy as np
 import xarray as xr
-
-from gfdlvitals.util.average import RichVariable
-from gfdlvitals.util.average import process_var
 
 import gfdlvitals.util.gmeantools as gmeantools
 import gfdlvitals.util.netcdf as netcdf
 
-__all__ = ["average", "xr_average"]
+
+__all__ = ["xr_average"]
 
 
 def xr_average(fyear, tar, modules):
+    """xarray-based processing routines for cubed sphere LM4 land output
+
+    Parameters
+    ----------
+    fyear : str
+        Year being processed (YYYY)
+    tar : tarfile
+        In-memory tarfile object
+    modules : dict
+        Mappings of netCDF file names inside the tar file to output db file names
+    """
+
     members = [
         x for x in modules if netcdf.tar_member_exists(tar, f"{fyear}.{x}.tile1.nc")
     ]
@@ -130,76 +134,3 @@ def xr_average(fyear, tar, modules):
                 gmeantools.xr_to_db(
                     _dset_weighted, fyear, f"{fyear}.{region}Ave{modules[member]}.db"
                 )
-
-
-def average(grid_file, data_file, fyear, out, lab):
-    """Mid-level averaging routine
-
-    Parameters
-    ----------
-    grid_file : list of bytes
-        Gridspec tiles
-    data_file : list of bytes
-        Data tiles
-    fyear : str
-        Year being processed
-    out : str
-        Output path directory
-    lab : [type]
-        DB file name
-    """
-    _grid_file = [netcdf.in_mem_nc(x) for x in grid_file]
-    _data_file = [netcdf.in_mem_nc(x) for x in data_file]
-
-    for land_file in [_data_file, _grid_file]:
-        if "geolat_t" in land_file[0].variables:
-            geolat = gmeantools.cube_sphere_aggregate("geolat_t", _data_file)
-            geolon = gmeantools.cube_sphere_aggregate("geolon_t", _data_file)
-            break
-
-    area_types = {}
-    for land_file in [_data_file, _grid_file]:
-        for variable in sorted(land_file[0].variables):
-            if re.match(r".*_area", variable) or re.match(r"area.*", variable):
-                # for now, skip the area variables that depend on time
-                timedependent = False
-                for dimension in land_file[0].variables[variable].dimensions:
-                    timedependent = (
-                        timedependent
-                        or land_file[0].dimensions[dimension].isunlimited()
-                    )
-                if not timedependent:
-                    if variable not in area_types.keys():
-                        area_types[variable] = gmeantools.cube_sphere_aggregate(
-                            variable, land_file
-                        )
-
-    depth = _data_file[0].variables["zhalf_soil"][:]
-    cell_depth = []
-    for i in range(1, len(depth)):
-        thickness = round((depth[i] - depth[i - 1]), 2)
-        cell_depth.append(thickness)
-    cell_depth = np.array(cell_depth)
-
-    variables = list(_data_file[0].variables.keys())
-    variables = [
-        RichVariable(
-            x,
-            grid_file,
-            data_file,
-            fyear,
-            out,
-            lab,
-            geolat,
-            geolon,
-            area_types=area_types,
-            cell_depth=cell_depth,
-        )
-        for x in variables
-    ]
-
-    _ = [x.close() for x in _grid_file]
-    _ = [x.close() for x in _data_file]
-
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    pool.map(partial(process_var, **{"averager": "land_lm4"}), variables)

gfdlvitals/averagers/latlon.py
@@ -1,23 +1,29 @@
 """Averaging utilities for lat-lon data"""
 
-import multiprocessing
-
-from functools import partial
 
 import numpy as np
-
-from gfdlvitals.util.average import RichVariable
-from gfdlvitals.util.average import process_var
+import xarray as xr
 
 import gfdlvitals.util.gmeantools as gmeantools
 import gfdlvitals.util.netcdf as netcdf
 
-import xarray as xr
 
-__all__ = ["average", "xr_average"]
+__all__ = ["xr_average"]
 
 
 def xr_average(fyear, tar, modules):
+    """xarray-based processing routines for lat-lon model output
+
+    Parameters
+    ----------
+    fyear : str
+        Year being processed (YYYY)
+    tar : tarfile
+        In-memory tarfile object
+    modules : dict
+        Mappings of netCDF file names inside the tar file to output db file names
+    """
+
     members = [x for x in modules if netcdf.tar_member_exists(tar, f"{fyear}.{x}.nc")]
 
     for member in members:
@@ -55,44 +61,3 @@ def xr_average(fyear, tar, modules):
             gmeantools.xr_to_db(
                 _dset_weighted, fyear, f"{fyear}.{region}Ave{modules[member]}.db"
             )
-
-
-def average(grid_file, data_file, fyear, out, lab):
-    """Mid-level averaging routine
-
-    Parameters
-    ----------
-    grid_file : bytes-like
-        Gridspec dataset
-    data_file : bytes-like
-        Model output dataset
-    fyear : str
-        Year being processed
-    out : str
-        Output path directory
-    lab : [type]
-        DB file name
-    """
-
-    _grid_file = netcdf.in_mem_nc(grid_file)
-    _data_file = netcdf.in_mem_nc(data_file)
-
-    lat = _grid_file.variables["lat"][:]
-    lon = _grid_file.variables["lon"][:]
-
-    geolat = np.tile(lat[:, None], (1, lon.shape[0]))
-    geolon = np.tile(lon[None, :], (lat.shape[0], 1))
-    cell_area = gmeantools.standard_grid_cell_area(lat, lon)
-
-    variables = list(_data_file.variables.keys())
-    variables = [
-        RichVariable(
-            x, grid_file, data_file, fyear, out, lab, geolat, geolon, cell_area
-        )
-        for x in variables
-    ]
-
-    _ = [x.close for x in [_grid_file, _data_file]]
-
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    pool.map(partial(process_var, **{"averager": "lat-lon"}), variables)