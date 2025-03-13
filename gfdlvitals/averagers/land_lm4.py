8f4f38ccc7ea3bb3be47fb97cc7ca6edc6b79c5a
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2025-03-13 16:43:54 -0400

    Updated t_bounds indexing from [:,1] to use isel and nv=1 or nv=0 to use proper xarray subsetting.

--- gfdlvitals/averagers/land_lm4.py
+++ gfdlvitals/averagers/land_lm4.py
@@ -112,7 +112,7 @@ def xr_average(fyear, tar, modules):
                 # _masked_area = _masked_area.fillna(0)
 
                 t_bounds = dset.time_bnds
-                dt = t_bounds[:,1] - t_bounds[:,0]
+                dt = t_bounds.isel(nv=1) - t_bounds.isel(nv=0)
                 weights = dt.astype("float") * _masked_area
                 if _measure == "soil_area":
                     area_x_depth = _masked_area * depth
